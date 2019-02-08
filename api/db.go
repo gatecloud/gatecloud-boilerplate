42d8b3d177faca7d068301d19cf4927c308335b4
OscarZhou <zhouhongyu1989@gmail.com>
2019-02-08 16:51:45 +1300

    Integrate library config into project

--- api/db.go
+++ api/db.go
@@ -8,7 +8,7 @@ import (
 
 // InitDB creates the connection to database and returns a db handler
 func InitDB() (*gorm.DB, error) {
-	db, err := gorm.Open(configs.Configuration.Env.DbEngine, configs.Configuration.Env.DbConn)
+	db, err := gorm.Open(configs.Configuration.DbEngine, configs.Configuration.DbConn)
 	if err != nil {
 		return nil, err
 	}
